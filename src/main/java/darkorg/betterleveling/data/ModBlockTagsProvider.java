e0c590a93d69bfdb46277bae7775f0823d90dc80
Darkorg69 <k.mikaelson78@gmail.com>
2022-11-28 01:59:23 +0200

    Update 1.18.2-0.3.1
    -Added Forge "raw_materials" ItemTag to Prospeting, now should drop bonus Copper, Iron and Gold and with any modded ores which apply the "raw_materials" ItemTag to it's ore drops.

--- src/main/java/darkorg/betterleveling/data/ModBlockTagsProvider.java
+++ src/main/java/darkorg/betterleveling/data/ModBlockTagsProvider.java
@@ -8,7 +8,6 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
-@SuppressWarnings("unchecked")
 public class ModBlockTagsProvider extends BlockTagsProvider {
     public ModBlockTagsProvider(DataGenerator generatorIn, String modId, ExistingFileHelper existingFileHelper) {
         super(generatorIn, modId, existingFileHelper);
@@ -17,7 +16,7 @@ public class ModBlockTagsProvider extends BlockTagsProvider {
     @Override
     protected void addTags() {
         tag(ModTags.Blocks.CROPS).add(Blocks.COCOA, Blocks.NETHER_WART).addTag(BlockTags.CROPS);
-        tag(ModTags.Blocks.ORES).addTags(Tags.Blocks.ORES_COAL, Tags.Blocks.ORES_DIAMOND, Tags.Blocks.ORES_EMERALD, Tags.Blocks.ORES_LAPIS, Tags.Blocks.ORES_QUARTZ, Tags.Blocks.ORES_REDSTONE);
+        tag(ModTags.Blocks.ORES).addTag(Tags.Blocks.ORES);
         tag(ModTags.Blocks.TREASURE_BLOCKS).addTag(BlockTags.DIRT);
     }
 }
--- src/main/java/darkorg/betterleveling/data/ModItemTagsProvider.java
+++ src/main/java/darkorg/betterleveling/data/ModItemTagsProvider.java
@@ -20,7 +20,7 @@ public class ModItemTagsProvider extends ItemTagsProvider {
         tag(ModTags.Items.ANIMAL_MEAT).add(Items.BEEF, Items.CHICKEN, Items.MUTTON, Items.PORKCHOP, Items.RABBIT, Items.ROTTEN_FLESH);
         tag(ModTags.Items.ANIMAL_SKIN).add(Items.FEATHER, Items.RABBIT_HIDE).addTags(ItemTags.WOOL, Tags.Items.LEATHER);
         tag(ModTags.Items.CROPS).add(Items.COCOA_BEANS, Items.SWEET_BERRIES).addTags(Tags.Items.CROPS);
-        tag(ModTags.Items.ORES).add(Items.COAL, Items.REDSTONE).addTags(Tags.Items.GEMS, Tags.Items.NUGGETS);
+        tag(ModTags.Items.ORES).add(Items.COAL, Items.REDSTONE).addTags(Tags.Items.GEMS, Tags.Items.NUGGETS, Tags.Items.RAW_MATERIALS);
         tag(ModTags.Items.TREASURE_COMMON).add(Items.CLAY_BALL).addTags(Tags.Items.STRING);
         tag(ModTags.Items.TREASURE_RARE).add(Items.GOLDEN_SHOVEL).addTags(Tags.Items.GEMS, Tags.Items.RODS);
         tag(ModTags.Items.TREASURE_UNCOMMON).add(Items.ARROW).addTags(Tags.Items.DUSTS, Tags.Items.NUGGETS);
